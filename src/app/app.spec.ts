8998093dda20a9a93401310630eef343e24c85b0
Alexandre Barreto <plotartemail@gmail.com>
2026-08-07 19:20:22 -0300

    fix: ajusta budget Angular para 700kB e corrige configuração dos testes unitários com Vitest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,22 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+import { RouterModule } from '@angular/router';
 import { App } from './app';
-import { AppModule } from './app-module';
+
+@Component({ selector: 'app-toast', template: '', standalone: false })
+class MockToast {}
+
+@Component({ selector: 'app-modal', template: '', standalone: false })
+class MockModal {}
+
+@Component({ selector: 'app-detail-modal', template: '', standalone: false })
+class MockDetailModal {}
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppModule]
+      declarations: [App, MockToast, MockModal, MockDetailModal],
+      imports: [RouterModule.forRoot([])]
     }).compileComponents();
   });
 
